7cf80a6137e710a370380b089b334c1ff6881c7d
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-08-11 11:02:46 -0700

    Added app component, changed pathing and installed bootstrap.

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css'
+
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        };
+    }
+
+    render(){
+        return(
+            <div className = "container-fluid">
+                <h2>title</h2>
+            </div>
+        )
+    }
+}
+
+export default App
