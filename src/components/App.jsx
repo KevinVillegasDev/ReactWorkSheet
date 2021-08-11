01347d9efc5a3f032a507f8fed7254d8d3f32be8
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-08-11 15:45:44 -0700

    Created superhero table component and styled table.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import DisplayName from './DisplayName/DisplayName';
 import NamesList from './NamesList/NamesList';
 import AlertUser from './AlertUser/AlertUser';
+import SuperheroTable from './SuperheroTable/SuperheroTable';
 
 class App extends Component {
     constructor(props){
@@ -11,7 +12,27 @@ class App extends Component {
             firstName: 'Reggie',
             lastName: 'White',
             nameIndex: 0,
-            names: ['Mike', 'Nevin', 'Aaron', 'Tory', 'Kelly']
+            names: ['Mike', 'Nevin', 'Aaron', 'Tory', 'Kelly'],
+            superheroes: [
+                {
+                    superheroId: 1,
+                    name: 'Batman',
+                    primaryAbility: 'Wealthy',
+                    secondaryAbility: 'Rich'
+                },
+                {
+                    superheroId: 2,
+                    name: 'Superman',
+                    primaryAbility: 'Super strength',
+                    secondaryAbility: 'Fly'
+                },
+                {
+                    superheroId: 3,
+                    name: 'Spiderman',
+                    primaryAbility: 'Spider senses',
+                    secondaryAbility: 'Shoots web'
+                }
+            ]
         };
     }
 
@@ -31,6 +52,7 @@ class App extends Component {
                     <NamesList displayNameList={this.state.names}/>
                 </ol>
                 <AlertUser alert={this.AlertTheUser}/>
+                <SuperheroTable superTable={this.state.superheroes}/>
             </div>
 
         )
