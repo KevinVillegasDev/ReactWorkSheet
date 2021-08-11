415facd11ae2b13f84c603a4c2ad73444f538f25
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-08-11 11:44:27 -0700

    Refactored logic inside render function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import './App.css'
+import DisplayName from './DisplayName/DisplayName';
 
 class App extends Component {
     constructor(props){
         super(props)
         this.state = {
-
+            firstName: 'Reggie',
+            lastName: 'White'
         };
     }
 
     render(){
         return(
             <div className = "container-fluid">
-                <h2>title</h2>
+                <h1>
+                    <DisplayName firstName={this.state.firstName} lastName={this.state.lastName}/>
+                </h1>
             </div>
         )
     }
